ad467d34e7f8a858af5d62064a5ddcdc52355bdb
Mina <minaschool11@gmail.com>
2024-07-30 08:04:13 +0300

    Made #guess

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,15 +1,26 @@
 class Hangman
-  attr_accessor :secret_word, :words
+  attr_accessor :secret_word, :words, :letters
 
   def initialize
     self.words = File.read('google-10000-english-no-swears.txt').split
     self.secret_word = generate_secret_word
+    self.letters = ('a'..'z').to_a
   end
 
   def generate_secret_word
     loop do
       secret_word = words.sample
       return secret_word if secret_word.length.between?(5, 12)
+    end
+  end
+
+  def guess
+    loop do
+      print 'Enter a guess: '
+      letter = gets.chomp
+      return letter if letters.include? letter
+
+      puts 'Please a valid guess.'
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 require_relative 'lib/hangman'
 
 hangman = Hangman.new
-puts hangman.secret_word
-puts hangman.secret_word.length
+hangman.guess
